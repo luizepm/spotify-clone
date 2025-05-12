5a84c395efd20752d660b540fe1afa23cfec76cc
luizepm <luizepm@hotmail.com>
2025-05-11 21:59:23 -0300

    update: deploy code

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import { db } from "./connect.js";
 import { artistArray } from "../../front-end/src/assets/database/artists.js";
 import { songsArray } from "../../front-end/src/assets/database/songs.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
@@ -10,24 +13,32 @@ const PORT = 3001;
 app.use(cors());
 // app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
 
-app.get("/artists_json", async (request, response) => {
+app.get("/api/artists_json", async (request, response) => {
   response.send(artistArray);
 });
 
-app.get("/songs_json", async (request, response) => {
+app.get("/api/songs_json", async (request, response) => {
   response.send(songsArray);
+});
+
+// Configuro o express para servir arquivos estáticos do front-end
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+// Qualquer outra url, vai direcionar para index
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
